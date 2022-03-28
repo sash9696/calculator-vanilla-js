b5c3aac9f364101b26a3915fb35b45f0fbe8f6e2
sash9696 <sahil@Sahils-MacBook-Air.local>
2022-03-28 12:33:29 +0530

    built simple calculator using vanilla js

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let display = document.getElementById('display')
+let buttons = Array.from(document.getElementsByClassName('button'))
+let arr = []
+buttons.map(button => {
+    button.addEventListener('click', (e)=>{
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = ''
+                arr = []
+                break
+            case '=':
+                try{
+                     console.log(arr.join(''))
+                    display.innerText = eval(arr.join(''))
+                    
+                }
+                catch{
+                    display.innerText = 'Error'
+                }
+                break
+            case '<-':
+                if(display.innerText ){
+                    display.innerText = arr.join('').slice(0,-1)
+                    arr.pop()
+
+                }
+                break
+            default:
+                display.innerText = e.target.innerText
+                arr.push(e.target.innerText)
+                
+        }
+    })
+})
